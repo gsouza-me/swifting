63f2d7154261af95766a0a6fff45a6aeefaba02f
mkxliv <guii_souza@live.com>
2022-09-15 21:44:34 -0300

    Create a Structure folder + modelData

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,37 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Gui  on 15/09/22.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable{
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image{
+        Image(imageName)
+    }
+    
+    private var coordinates: Coodinates
+    
+    var locationCoodinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coodinates: Hashable, Codable{
+        var latitude: Double
+        var longitude: Double
+    }
+    
+}
---
+++ Landmarks/Model/modelData.swift
@@ -0,0 +1,35 @@
+//
+//  modelData.swift
+//  Landmarks
+//
+//  Created by Gui  on 15/09/22.
+//
+
+import Foundation
+
+var landamarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>( _ filename: String ) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else{
+        fatalError("Não foi possivel encontrar \(filename) no pacote principal.")
+        
+    }
+    
+    do{
+        data = try Data(contentsOf: file)
+    } catch{
+        fatalError("Não foi possivel carregar \(filename) do pacote principal.\(error).")
+    }
+    
+    do{
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }catch{
+        fatalError("Não foi possivel juntar \(filename) como \(T.self):\n\(error)")
+    }
+            
+    
+}
